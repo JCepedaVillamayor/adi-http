29ef229d827c0e60294e177e64ca763320889516
jcepedavillamayor <jcepedavillamayor@gmail.com>
2016-10-15 21:51:10 +0200

    Step 2 function implemeted
    Although the function has been implemented, it does not work
    due to a 405 status code

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 MAIN_URL = "http://httptask-142719.appspot.com/{dni}"
 
@@ -8,12 +9,23 @@ def step1(url):
     if req.status_code == 200:
         print(req.json())
         return req.json()["value"], req.json()["next"]
+
+def step2(url, value, next_op):
+    step2_url = url + next_op
+    print(step2_url)
+    headers = {'Content-type': 'application/json'}
+    payload = {"value": value}
+    req = requests.post(step2_url, json=payload, headers=headers)
+    print(req.status_code)
+    print(req.text)
     
 def main():
     print("What is your DNI (Do not put your last letter)")
-    dni = input()
+    dni = "70360133" #input()
     main_url = MAIN_URL.format(dni=dni)
     stp1_value, stp1_next_op = step1(main_url)
-
+    step2(main_url, stp1_value, stp1_next_op)
+    
 if __name__ == "__main__":
     main()
+main()
